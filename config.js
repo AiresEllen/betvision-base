d52240ff750f5a5824cea8c25de6f193f0cfecfd
AiresEllen <ellen.costa.aires@gmail.com>
2026-04-02 18:14:03 -0300

    Cola URL copiada do Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://ingnvqvxmnbtwgnwpyxgn.supabase.co",
+  SUPABASE_URL: "https://ingnvqvxmbtwgnwpyxgn.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_-6hJ7Cb6JwPonU6PsDFDfQ_orMDE0zT",
 };
